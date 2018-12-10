54076c67af1e25406c4a595e8b0afef9414fbae0
Iftifar Taz <iftifar.taz@northsouth.edu>
2018-12-11 01:44:04 +0600

    new functionalities

--- src/App.js
+++ src/App.js
@@ -1,36 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
-import lottery from './lottery-contract';
+import React, { Component } from "react";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery-contract";
 
 class App extends Component {
+	state = {
+		manager: "",
+		lastWinner: "",
+		players: [],
+		balance: "",
+		value: "",
+		message: ""
+	};
 
-	constructor(props) {
-		super(props);
+	setValueState = value => {
+		this.setState({ value: value });
+	};
 
-		this.state = {
-			manager: ''
-		};
-	}
-
-	async componentDidMount() {
-		const manager = await lottery.methods.manager().call();
+	onSubmit = async event => {
+		event.preventDefault();
+		const accounts = await web3.eth.getAccounts();
 
 		this.setState({
-			manager: manager
+			message: "Waiting on transaction success..."
 		});
-	}
 
-	render() {
-		console.log(lottery);
+		await lottery.methods.enter().send({
+			from: accounts[0],
+			value: web3.utils.toWei(this.state.value, "ether")
+		});
+
+		this.setState({
+			players: await lottery.methods.getPlayers().call(),
+			balance: await web3.eth.getBalance(lottery.options.address),
+			message: "You have been entered!"
+		});
+	};
+
+	onClick = async () => {
+		const accounts = await web3.eth.getAccounts();
+
+		this.setState({
+			message: "Waiting on transaction success..."
+		});
+
+		await lottery.methods.pickWinner().send({
+			from: accounts[0]
+		});
+
+		this.setState({
+			lastWinner: await lottery.methods.lastWinner().call(),
+			players: [],
+			balance: "",
+			message: "A winner has been picked!"
+		});
+	};
+
+	componentDidMount = async () => {
+		this.setState({
+			manager: await lottery.methods.manager().call(),
+			players: await lottery.methods.getPlayers().call(),
+			balance: await web3.eth.getBalance(lottery.options.address)
+		});
+	};
+
+	render = () => {
 		return (
 			<div>
 				<h2>Lottery Contract!!</h2>
 				<p>This contract is managed by {this.state.manager}</p>
+				<p>Last winner is {this.state.lastWinner}</p>
+				<p>There are currently {this.state.players.length} people entered.</p>
+				<p>
+					Competing to win {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+					ether!
+        </p>
+				<hr />
+				<form onSubmit={this.onSubmit}>
+					<h4>Want to try your luck?</h4>
+					<div>
+						<label>Amount of ether to enter: </label>
+						<input
+							value={this.state.value}
+							onChange={event => this.setValueState(event.target.value)}
+						/>
+					</div>
+					<button>Enter</button>
+				</form>
+				<hr />
+				<h4>Ready to pick a winner?</h4>
+				<button onClick={this.onClick}>Pick a winner!</button>
+				<hr />
+				<h1>{this.state.message}</h1>
 			</div>
 		);
-	}
+	};
 }
 
 export default App;
